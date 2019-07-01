b399cc0e163b8766630a6943152d95ccc7c5b395
Stefan Davis <stefan@turnkeylinux.org>
2019-07-01 05:48:44 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-paged',
+    version='1.0',
+    description='Python utility for paging text',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-paged',
+    packages=['paged'])
